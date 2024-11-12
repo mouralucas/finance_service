4c365f0a5c2ec5cb83a7e4be6cb4ce21f442a193
Lucas Moura <lucaspenha471@gmail.com>
2024-11-11 21:24:45 -0300

    Add DefaultModel schema to all account request schemas

--- schemas/request/account.py
+++ schemas/request/account.py
@@ -8,7 +8,7 @@ from pydantic.alias_generators import to_camel, to_snake
 from rolf_common.schemas.base import DefaultModel
 
 
-class CreateAccountRequest(BaseModel):
+class CreateAccountRequest(DefaultModel):
     bank_id: uuid.UUID = Field(..., alias="bankId", description="The id of the bank")
     nickname: str = Field(..., alias="nickname", description="The nickname of the account")
     description: str = Field(None, alias="description", description="The description of the account")
@@ -20,25 +20,24 @@ class CreateAccountRequest(BaseModel):
     currency_id: str = Field(..., alias="currencyId", description="The currency of the account")
 
 
-class CloseAccountRequest(BaseModel):
+class CloseAccountRequest(DefaultModel):
     id: uuid.UUID = Field(..., alias="accountId", description="The id of the account")
-    close_date: datetime.date = Field(None, alias="closeDate", description="The close date of the account")
+    close_date: datetime.date = Field(None, description="The close date of the account")
 
 
-class GetAccountRequest(BaseModel):
+class GetAccountRequest(DefaultModel):
     id: uuid.UUID | None = Field(Query(None, alias="accountId", description="The id of the account"))
-    currency_id: str | None = Field(Query(None, alias="currencyId", description="The currency of the account"))
+    currency_id: str | None = Field(Query(None, description="The currency of the account"))
     active: bool = Field(Query(True, description="Whether the account is active"))
 
 
 class CreateAccountTransactionRequest(DefaultModel):
-
     account_id: uuid.UUID = Field(..., description="The id of the account")
     currency_id: str = Field(..., description="The currency of the account")
     amount: Decimal = Field(..., description="The amount of the transaction in the account currency")
     transaction_date: datetime.date = Field(..., description="The date of the transaction")
     category_id: uuid.UUID = Field(..., description="The id of the category")
-    description: str = Field(None,  description="The description of the transaction")
+    description: str = Field(None, description="The description of the transaction")
 
     transaction_currency_id: str = Field(None, description="The currency of the transaction")
     transaction_amount: Decimal = Field(None, description="The amount in the transaction currency")
@@ -53,13 +52,8 @@ class CreateAccountTransactionRequest(DefaultModel):
     is_validated: bool = Field(None, description="Whether the transaction is validated by the user")
 
 
-class UpdateAccountTransactionRequest(BaseModel):
-    model_config = ConfigDict(from_attributes=True, alias_generator=AliasGenerator(
-        alias=to_camel,
-        serialization_alias=to_camel,
-    ))
-
-    id: int = Field(...,  alias='transactionId', description="The id of the account")
+class UpdateAccountTransactionRequest(DefaultModel):
+    id: int = Field(..., alias='transactionId', description="The id of the account")
     account_id: uuid.UUID | None = Field(None, description="The id of the account")
     currency_id: str | None = Field(None, description="The currency of the account")
     amount: Decimal | None = Field(None, description="The amount of the transaction in the account currency")
@@ -79,12 +73,12 @@ class UpdateAccountTransactionRequest(BaseModel):
     is_validated: bool | None = Field(None, description="Whether the transaction is validated by the user")
 
 
-class CreateBalanceRequest(BaseModel):
-    account_id: uuid.UUID = Field(None, alias='accountId', description='The id of the account')
-    start_period: int = Field(None, alias='startPeriod', description='The start period of the balance')
+class CreateBalanceRequest(DefaultModel):
+    account_id: uuid.UUID = Field(None, description='The id of the account')
+    start_period: int = Field(None, description='The start period of the balance')
 
 
-class GetBalanceRequest(BaseModel):
-    account_id: uuid.UUID = Field(None, alias='accountId', description='The id of the account')
-    start_period: int | None = Field(None, alias='startPeriod', description='The start period of the balance')
-    end_period: int | None = Field(None, alias='endPeriod', description='The end period of the balance')
+class GetBalanceRequest(DefaultModel):
+    account_id: uuid.UUID = Field(None, description='The id of the account')
+    start_period: int | None = Field(None, description='The start period of the balance')
+    end_period: int | None = Field(None, description='The end period of the balance')
