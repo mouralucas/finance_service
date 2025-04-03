09203eb68da92ff6e08340e8c9a29f9f46efdabb
Lucas Moura <lucaspenha471@gmail.com>
2025-04-02 23:00:40 -0300

    Adjust typo in InvestmentStatementSchema att

--- schemas/investment.py
+++ schemas/investment.py
@@ -120,7 +120,7 @@ class InvestmentStatementSchema(BaseModel):
     id: uuid.UUID = Field(..., serialization_alias='investmentStatementId', description='The id of the statement')
     investment_id: uuid.UUID = Field(..., description='The id of the investment')
     investment: InvestmentSchema | None = Field(None, description='The object of the investment')
-    referenceDate: date = Field(..., description='The date of the investment')
+    reference_date: date = Field(..., description='The date of the investment')
     period: int = Field(..., description='The period of the statement')
     gross_amount: float = Field(..., description='The gross amount of the investment in the period')
     total_tax: float = Field(..., description='The total tax amount of the investment in the period')
