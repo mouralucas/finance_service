5a16d8847845959c7573d0929305152622695991
Lucas Moura <lucaspenha471@gmail.com>
2025-04-29 09:32:27 -0300

    Fix import in populate_database.py

--- populate_database.py
+++ populate_database.py
@@ -4,9 +4,11 @@ from rolf_common.managers import BaseDataManager
 
 from backend.database import sessionmanager
 from data_mock.account import get_account_type_mock, get_open_account_mock, get_closed_account_mock, get_account_transaction_mock
-from data_mock.core import get_bank_mock, get_currency_mock, get_index_type_mock, get_indexer_mock, get_category_mock, get_liquidity_mock, get_country_mock, get_expense_type_mock, get_category_parent_mock, get_tax_mock, get_fee_mock, \
-    get_brazilian_fund_mock
+from data_mock.core import (get_bank_mock, get_currency_mock, get_index_type_mock, get_indexer_mock,
+                            get_category_mock, get_liquidity_mock, get_country_mock, get_expense_type_mock,
+                            get_category_parent_mock, get_tax_mock, get_fee_mock)
 from data_mock.credit_card import get_credit_card_mock, get_cancelled_credit_card_mock
+from data_mock.investment.base import get_brazilian_fund_mock
 from data_mock.investment.investment import get_fixed_income_br_investment_type_mock, get_investment_mock, get_investment_statement_mock, get_investment_category_mock
 from data_mock.investment.brazilian_funds import get_brazilian_fund_investment_mock
 from models.account import AccountTypeModel, AccountModel, AccountTransactionModel
@@ -48,6 +50,7 @@ async def populate():
         await BaseDataManager(session).add_or_ignore_all(InvestmentModel, get_investment_mock())
         await BaseDataManager(session).add_or_ignore_all(InvestmentStatementModel, get_investment_statement_mock())
         await BaseDataManager(session).add_or_ignore_all(FundsBrModel, get_brazilian_fund_mock())
+
 
 if __name__ == '__main__':
     loop = asyncio.run(populate())
