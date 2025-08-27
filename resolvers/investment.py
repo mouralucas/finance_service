10410e2bd310cd0a0b8beff27c9d6ef619fe6356
Lucas Moura <lucaspenha471@gmail.com>
2025-08-27 17:28:22 -0300

    Added get investment statement to graphql

--- resolvers/investment.py
+++ resolvers/investment.py
@@ -1,7 +1,7 @@
 
 
 from ariadne import MutationType, QueryType
-from schemas.request.investment import GetInvestmentRequest
+from schemas.request.investment import GetInvestmentRequest, GetStatementRequest
 from services.investment import InvestmentService
 
 
@@ -13,5 +13,13 @@ async def get_investments_resolver(_, info, params):
     return investments.model_dump(by_alias=True)
 
 
+async def get_investment_statements_resolver(_, info, params):
+    params_ = GetStatementRequest.model_validate(params)
+
+    statements = await InvestmentService(session=info.context['session'], user=info.context['user']).get_statement(params=params_)
+
+    return statements.model_dump(by_alias=True)
+
 def bind_investment_resovlers(query: QueryType, mutation: MutationType):
     query.set_field('getInvestments', resolver=get_investments_resolver)
+    query.set_field('getInvestmentStatements', resolver=get_investment_statements_resolver)
