da3086e0ebf20c3ee4b2bbde6ddff4e182bf069d
Lucas Moura <lucaspenha471@gmail.com>
2025-09-09 10:37:48 -0300

    Added get account transactions to graphql

--- resolvers/account.py
+++ resolvers/account.py
@@ -3,6 +3,7 @@ from ariadne import MutationType, QueryType
 from schemas.request.account import (
     CreateAccountTransactionRequest,
     GetAccountRequest,
+    GetAccountTransactionRequest,
     GetBalanceRequest,
     UpdateAccountTransactionRequest,
 )
@@ -20,7 +21,13 @@ async def get_accounts_resolver(_, info, params):
 
 
 async def get_account_transactions(_, info, params):
-    pass
+    params = GetAccountTransactionRequest.model_validate(params)
+    
+    transactions = await AccountService(
+        session=info.context["session"], user=info.context["user"]
+    ).get_transactions(params=params)
+    
+    return transactions.model_dump(by_alias=True)
 
 
 async def create_account_transactions_resolver(_, info, transaction):
@@ -55,6 +62,7 @@ async def get_account_balance_resolver(_, info, params):
 
 def bind_account_resolvers(query: QueryType, mutation: MutationType):
     query.set_field("getAccounts", resolver=get_accounts_resolver)
+    query.set_field("getAccountTransactions", resolver=get_account_transactions)
     query.set_field("getAccountBalance", resolver=get_account_balance_resolver)
 
     mutation.set_field(
